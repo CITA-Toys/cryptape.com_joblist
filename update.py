1c79998188b4c5c9baab4b8c63fb84b02d50cc95
Yao <yao@cryptape.com>
2018-08-27 19:01:15 +0800

    update: add os.environ

--- update.py
+++ update.py
@@ -10,14 +10,14 @@ import qiniu.config
 
 class Config(str, enum.Enum):
     # 需要填写你的 Access Key 和 Secret Key
-    access_key = sys.argv[1]
-    secret_key = sys.argv[2]
+    access_key = os.environ['g_access_key']
+    secret_key = os.environ['g_secret_key']
     # 要上传的空间
-    bucket_name = sys.argv[3]
+    bucket_name = os.environ['g_bucket_name']
     # 上传到七牛后保存的文件名
-    file = sys.argv[4]
+    file = os.environ['g_file']
     # 域名地址
-    link = sys.argv[5]
+    link = os.environ['g_link']
 
 
 def update(auth):
